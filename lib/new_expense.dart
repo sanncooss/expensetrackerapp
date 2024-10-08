80992d80b6b58dc0a1821c37db9aed5bf0b9e9c1
ssannccoss <sanjarbekalisherov837@gmail.com>
2024-10-08 12:50:08 +0500

    Updated

---
+++ lib/new_expense.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+    );
+  }
+}
