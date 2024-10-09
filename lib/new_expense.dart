64918f6d0b3a9392cdf05481e9f8fa9d4f5c8112
ssannccoss <sanjarbekalisherov837@gmail.com>
2024-10-09 09:57:38 +0500

    Updated

--- lib/new_expense.dart
+++ lib/new_expense.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/expense.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -11,6 +15,36 @@ class _NewExpenseState extends State<NewExpense> {
   // var _enteredITtle = '';
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      firstDate: firstDate,
+      lastDate: now,
+      initialDate: now,
+    );
+    setState(() {
+      _selectedDate =
+          pickedDate; // here we are updating our picked date which is initially empty and assigning it to the DateTime variable
+    });
+  }
+
+  void _submitExpenseData() {
+    final enteredAmount =
+        double.tryParse(_amountController.text); //tryParse('Hello') => null
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      //show error message
+    }
+  }
 
   @override
   void dispose() {
@@ -39,14 +73,16 @@ class _NewExpenseState extends State<NewExpense> {
           ),
           Row(
             children: [
-              TextField(
-                // onChanged: _saveTitle,
-                controller: _amountController,
+              Expanded(
+                child: TextField(
+                  // onChanged: _saveTitle,
+                  controller: _amountController,
 
-                maxLength: 50,
-                decoration: const InputDecoration(
-                  prefixText: '\$',
-                  label: Text('Amount'),
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                    prefixText: '\$',
+                    label: Text('Amount'),
+                  ),
                 ),
               ),
               SizedBox(
@@ -54,25 +90,55 @@ class _NewExpenseState extends State<NewExpense> {
               ),
               Expanded(
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text('Selected date'), // selected date
+                    Text(
+                      _selectedDate == null
+                          ? 'No date selected '
+                          : formatter.format(
+                              _selectedDate!,
+                            ),
+                    ), // selected date
                     IconButton(
-                      onPressed: () {},
+                      onPressed: _presentDatePicker,
                       icon: const Icon(
                         Icons.calendar_month,
                       ),
                     ),
                   ],
                 ),
-              )
+              ),
             ],
+          ),
+          SizedBox(
+            height: 15,
           ),
           Row(
             children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value:
+                              category, // will be stored internally to tell dart that we are selecting category
+                          child: Text(
+                            category.name.toUpperCase(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Save expense'),
               ),
               TextButton.icon(
